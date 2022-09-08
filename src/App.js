cc8103b74f64fcbf4c8c4d8f20747034abb9215f
Cuong98 <cuongkieu2898@gmail.com>
2022-09-08 14:32:41 +0700

    header UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+// swiper bundle styles
+import "swiper/css/bundle";
+// swiper core styles
+import "swiper/css";
 
-import './App.scss';
+import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./App.scss";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Routesv6 from "./config/Routesv6";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Catalog from "./pages/Catalog";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>     
+      <Header />
+      <Routesv6 />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
