92c17fda728eb61aa62f3eb3b94a3c4269f46adc
Cuong98 <cuongkieu2898@gmail.com>
2022-09-08 19:03:40 +0700

    movie card UI

---
+++ src/components/movie-list/MovieList.jsx
@@ -0,0 +1,52 @@
+import React,{useState,useEffect} from 'react'
+import PropTypes from 'prop-types';
+import './movie-list.scss';
+import {SwiperSlide,Swiper} from 'swiper/react';
+
+import {Link} from 'react-router-dom'
+import tmdbApi, { category } from '../../api/tmdbApi';
+import apiConfig from '../../api/apiConfig';
+import MovieCard from '../movie-card/MovieCard';
+const MovieList = props => {
+
+    const [items,setItems] = useState([])
+    useEffect(() =>{
+        const getList = async () =>{
+            let res = null;
+            const params ={};
+            if(props.type !== "similar"){
+                switch(props.category){
+                    case category.movie:
+                        res = await tmdbApi.getMoviesList(props.type,{params});
+                        break;
+                    default:
+                        res = await tmdbApi.getTvList(props.type,{params});
+                }
+            }else{
+                res = await tmdbApi.similar(props.category,props.id);
+            }
+            setItems(res.results)
+        }
+        getList();
+    },[])
+  return (
+    <div className='movie-list'>
+        <Swiper grabCursor={true} spaceBetween={10} slidesPerView={'auto'}>
+            {
+                items.map((item,index) =>(
+                    <SwiperSlide key={index}>
+                      <MovieCard item={item} category={props.category}/>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
+    </div>
+  )
+}
+
+MovieList.propTypes = {
+    category:PropTypes.string.isRequired,
+    type:PropTypes.string.isRequired,
+}
+
+export default MovieList
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,57 @@ import React from "react";
 
 import HeroSlide from "../components/hero-slide/HeroSlide";
 
+import {Link} from "react-router-dom"
+import { OutLineButton } from "../components/button/Button";
+import MovieList from "../components/movie-list/MovieList";
+import { category,movieType,tvType } from "../api/tmdbApi";
+
 const Home = () => {
   return (
     <>
       <HeroSlide />
+      <div className="container">
+        {/* trending slide */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to={"/movie"}>
+              <OutLineButton className="small" >View More</OutLineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.popular} />
+        </div>
+        {/* rated slide */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated Movie</h2>
+            <Link to={"/movie"}>
+              <OutLineButton className="small" >View More</OutLineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated} />
+        </div>
+        {/* tvshow slide */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV</h2>
+            <Link to={"/tv"}>
+              <OutLineButton className="small" >View More</OutLineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular} />
+        </div>
+        {/* tvshow slide */}
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated TV</h2>
+            <Link to={"/tv"}>
+              <OutLineButton className="small" >View More</OutLineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.top_rated} />
+        </div>
+      </div>
     </>
   );
 };
